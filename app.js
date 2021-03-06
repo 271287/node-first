ef5187307b43ce7ec7c4d0f1a67bbb92468f8948
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-03-06 15:27:07 +0100

    Ppl generator

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const os = require('os');
+//const os = require('os');
+const fs = require('fs');
 
-console.log('Platform: ', os.platform());
-console.log('Arch: ', os.arch());
-console.log('UserInfo: ', os.userInfo());
+const gender = ['F', 'M'];
+const maleNames = ['John', 'Tom', 'Bruce', 'Travis'];
+const femaleNames = ['Anna', 'Jane', 'Cathy', 'Trinity'];
+const lastNames = ['Smith', 'Johnson', 'Lee', 'Burn'];
+const people = [];
+
+const randChoice = (arr) => {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber];
+};
+
+for (i = 0; i < 20; i++) {
+    let personsName = '';  
+    const personsGender = randChoice(gender);
+
+    if (personsGender === 'M') {
+        personsName = randChoice(maleNames);
+    } else {
+        personsName = randChoice(femaleNames);
+    }
+    const personsLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    const personsAge = Math.floor(Math.random() * 60 + 18);
+
+    const newPerson = {
+        gender : personsGender,
+        name: personsName,
+        lastName: personsLastName,
+        age: personsAge,
+      };
+      people.push(newPerson);
+    
+};
+
+const peopleJSON = JSON.stringify(people);
+
+fs.writeFile('outputfile.txt', peopleJSON, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+
+
+
